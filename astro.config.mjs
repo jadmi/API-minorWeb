c43f2c5d78250bdc17e9a7c6704850cb2bb6894f
jadmi <152192404+jadmi@users.noreply.github.com>
2026-05-07 20:25:02 +0200

    adapter voor onrender

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  // Make the page on the server
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
